d949d42ed15419053908792346c954037ab820cc
Ivan Enzhaev <8observer8@gmail.com>
2019-11-17 20:43:25 +0400

    Simple Rhombuse

---
+++ Rhombuses/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Rhombuses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new Window())
+            {
+                window.Run();
+            }
+        }
+    }
+}
